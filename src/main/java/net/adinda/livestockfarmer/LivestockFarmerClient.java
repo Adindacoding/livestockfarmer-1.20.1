3be95708d821210f4e3c0f5f10387ed66b2cf632
adindacoding <adindabeer@gmail.com>
2024-01-25 13:41:58 +0100

    Initial commit

---
+++ src/main/java/net/adinda/livestockfarmer/LivestockFarmerClient.java
@@ -0,0 +1,10 @@
+package net.adinda.livestockfarmer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LivestockFarmerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/adinda/livestockfarmer/LivestockFarmerDataGenerator.java
@@ -0,0 +1,11 @@
+package net.adinda.livestockfarmer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LivestockFarmerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
